3792dba7a85752ba419af7419258979837c436b6
Yubeen Jo <jybin00@naver.com>
2021-11-16 17:19:36 +0900

    커밋을 자주 해야되는데 자꾸 까먹는다.

--- hands_software_challenge/Simulator/simul.py
+++ hands_software_challenge/Simulator/simul.py
@@ -30,14 +30,14 @@ class UserRobot(VacuumCleaner):
             postion=postion, 
             vision_sight=vision_sight
         )
-        self.fuel = fuel
         '''
             If you want to store some values, define your variables here.
             Using `self`-based variables, you can re-call the value of previous state easily.
         '''
         self.dir_x = 0
         self.dir_y = 0
-
+        self.detecting_obstacle = []
+        self.NOC = 0 # 배터리 충전 횟수
     def algorithms( self, grid_map: UserMap ):
         
         '''
@@ -95,123 +95,246 @@ class UserRobot(VacuumCleaner):
         left_side = -1
         upside = -1
         downside = 1
-        print(self.fuel)
+        battery = self._fuel
+
+        D_req_E = self.detecting_obstacle
+        D_req_E = [(grid_map.map[y][x].req_energy), # current location
+                   (grid_map.map[y][min(x+1, 16)].req_energy), # east
+                   (grid_map.map[y][max(x-1, 0)].req_energy),  # west
+                   (grid_map.map[min(y+1, 16)][x].req_energy), # south
+                   (grid_map.map[max(y-1, 0)][x].req_energy)]  # north
+        
+        def battery_charge():
+            print("moving for charge!")
+            self.mode = MOVE
+            if x == 8 and y == 8:
+                print("battery charged")
+                self.mode = CHAR
+                self.NOC = self.NOC + 1
+            if x > 8:
+                if y > 8:
+                    if D_req_E[2] != inf:
+                        self.dir_x, self.dir_y = left_side, 0
+                    else:
+                        self.dir_x, self.dir_y = 0, upside
+                else:
+                    if D_req_E[2] != inf:
+                        self.dir_x, self.dir_y = left_side, 0
+                    else:
+                        self.dir_x, self.dir_y = 0, downside  
+            if x < 8:
+                if y > 8:
+                    if D_req_E[1] != inf:
+                        self.dir_x, self.dir_y = right_side, 0
+                    else:
+                        self.dir_x, self.dir_y = 0, upside
+                else:
+                    if D_req_E[1] != inf:
+                        self.dir_x, self.dir_y = right_side, 0
+                    else:
+                        self.dir_x, self.dir_y = 0, downside
+            else:
+                if y > 8: 
+                    if D_req_E[4] != inf: self.dir_x, self.dir_y = 0, upside
+                    else: self.dir_x, self.dir_y = right_side, 0
+                if y < 8:
+                    if D_req_E[3] != inf: self.dir_x, self.dir_y = 0, downside
+                    else: self.dir_x, self.dir_y = right_side, 0
+                    
+                    
 
         self.max_h = grid_map.height - 1
         self.max_w = grid_map.width - 1
+        print("현재 위치 x: {}, y: {}".format(str(x),str(y)))     
             
-        if self.mode == STAY:
-            print("this car is staying...")
-            if grid_map.map[y+1][x].req_energy == inf:
-                print("downside required energy is inf!")
-                self.dir_x = 0
-                self.dir_y = upside
-                if grid_map.map[y][x].req_energy == 0:
-                    self.mode = MOVE
+#처음 시작 및 충전 후에        
+        def cleaning():
+            print("battery: {}".format(battery))   
+            if self.mode == STAY or CHAR:
+                print("this car is staying...")
+                if D_req_E[3] == inf:
+                    print("downside required energy is inf!")
+                    self.dir_x, self.dir_y = 0, upside
+                    if D_req_E[0] == 0: self.mode = MOVE
+                    else: self.mode = CLEN
+                    
+                elif D_req_E[4] == inf:
+                    print("upside required energy is inf!")
+                    self.dir_x, self.dir_y = 0, downside
+                    if D_req_E[0] == 0: self.mode = MOVE
+                    else: self.mode = CLEN
+                    
+                elif D_req_E[1] == inf:
+                    print("rightside required energy is inf!")
+                    self.dir_x, self.dir_y = left_side, 0
+                    if D_req_E[0] == 0: self.mode = MOVE
+                    else: self.mode = CLEN
+                    
+                elif D_req_E[2] == inf:
+                    print("leftside required energy is inf!")
+                    self.dir_x = right_side
+                    self.dir_y = 0
+                    if D_req_E[0] == 0: self.mode = MOVE
+                    else: self.mode = CLEN
+                    
                 else:
-                    self.mode = CLEN
-            if grid_map.map[y-1][x].req_energy == inf:
-                print("upside required energy is inf!")
-                self.dir_x = 0
-                self.dir_y = downside
-                if grid_map.map[y][x].req_energy == 0:
-                    self.mode = MOVE
-                else:
-                    self.mode = CLEN
-            if grid_map.map[y][x+1].req_energy == inf:
-                print("rightside required energy is inf!")
-                self.dir_x = left_side
-                self.dir_y = 0
-                if grid_map.map[y][x].req_energy == 0:
-                    self.mode = MOVE
-                else:
-                    self.mode = CLEN
-            if grid_map.map[y][x-1].req_energy == inf:
-                print("leftside required energy is inf!")
+                    print("there are not any obstacles!")
+                    if self.mode == CHAR:
+                        self.dir_x, self.dir_y = 0, downside
+                        if D_req_E[0] == 0: self.mode = MOVE
+                        else: self.mode = CLEN
+                    else : self.dir_x, self.dir_y = left_side, 0
+                    if D_req_E[0] == 0: self.mode = MOVE
+                    else: self.mode = CLEN
+# Each corner             
+            if x == 0 and y == 0:
+                print("left top corner!")
                 self.dir_x = right_side
                 self.dir_y = 0
-                if grid_map.map[y][x].req_energy == 0:
-                    self.mode = MOVE
-                else:
-                    self.mode = CLEN
-            else:
-                print("there are not any obstacles!")
-                self.dir_x = -1
-                self.dir_y = 0
-                if grid_map.map[y][x].req_energy == 0:
+                if D_req_E[0] == 0:
                     self.mode = MOVE
                 else:
                     self.mode = CLEN
                     
-        
-        if x == 0:
-            if grid_map.map[y+1][x].req_energy == inf: # 아래쪽에 장애물이 있으면 위로
-                print("downside required energy is inf!")
+            if x == 16 and y == 0:
+                print("right top corner!")
+                self.dir_x = 0
+                self.dir_y = downside
+                if D_req_E[0] == 0:
+                    self.mode = MOVE
+                else:
+                    self.mode = CLEN
+                    
+            if x == 0 and y == 16:
+                print("left down corner!")
                 self.dir_x = 0
                 self.dir_y = upside
-                if grid_map.map[y][x].req_energy == 0:
+                if D_req_E[0] == 0:
                     self.mode = MOVE
                 else:
                     self.mode = CLEN
-            if grid_map.map[y-1][x].req_energy == inf: # 위쪽에 장애물이 있으면 오른쪽으로
-                print("upside required energy is inf!")
-                self.dir_x = right_side
+            if x == 16 and y == 16:
+                print("right down corner!")
+                self.dir_x = left_side
                 self.dir_y = 0
-                if grid_map.map[y][x].req_energy == 0:
+                if D_req_E[0] == 0:
                     self.mode = MOVE
                 else:
                     self.mode = CLEN
-            else:
-                self.dir_x = upside
-                self.dir_y = 0
-                if grid_map.map[y][x].req_energy == 0:
-                    self.mode = MOVE
+
+# Each line            
+            if x == 0 and y > 0 and y < 16: # 왼쪽 끝이지만 모서리는 아닐 때
+                if D_req_E[3] == inf: # 아래쪽에 장애물이 있으면 위로
+                    print("downside required energy is inf!")
+                    self.dir_x = 0
+                    self.dir_y = upside
+                    if D_req_E[0] == 0:
+                        self.mode = MOVE
+                    else:
+                        self.mode = CLEN
+                elif D_req_E[4] == inf: # 위쪽에 장애물이 있으면 오른쪽으로
+                    print("upside required energy is inf!")
+                    self.dir_x = right_side
+                    self.dir_y = 0
+                    if D_req_E[0] == 0:
+                        self.mode = MOVE
+                    else:
+                        self.mode = CLEN
+                else: #아무것도 없으면 위쪽으로 
+                    self.dir_x = 0
+                    self.dir_y = upside
+                    if D_req_E[0] == 0:
+                        self.mode = MOVE
+                    else:
+                        self.mode = CLEN
+                    
+            if x == 16 and y > 0 and y < 16: # 오른쪽 끝까지 갔지만 모서리는 아닐 때
+                if D_req_E[3] == inf: # 아래쪽에 장애물이 있으면 왼쪽으로
+                    print("downside required energy is inf!")
+                    self.dir_x = left_side
+                    self.dir_y = 0
+                    if D_req_E[0] == 0:
+                        self.mode = MOVE
+                    else:
+                        self.mode = CLEN
+                elif D_req_E[4] == inf: # 위쪽에 장애물이 있으면 왼쪽으로
+                    print("upside required energy is inf!")
+                    self.dir_x = left_side
+                    self.dir_y = 0
+                    if D_req_E[0] == 0:
+                        self.mode = MOVE
+                    else:
+                        self.mode = CLEN
                 else:
-                    self.mode = CLEN
-                
-                
-        if x == 2 and y == 2:
-            self.dir_x = 1
-            self.dir_y = 0
-            if grid_map.map[y][x].req_energy == 0:
-                self.mode = MOVE
-            else:
-                self.mode = CLEN
+                    print("there are not any obstacle.") # 장애물 없으면 아래로.
+                    self.dir_x = 0
+                    self.dir_y = downside
+                    if D_req_E[0] == 0:
+                        self.mode = MOVE
+                    else:
+                        self.mode = CLEN
+                        
+            if y == 0 and x > 0 and x < 16: # 위쪽 끝까지 갔지만 모서리는 아닐 때
+                if D_req_E[2] == inf: # 왼쪽에 장애물이 있으면 아래로
+                    print("downside required energy is inf!")
+                    self.dir_x = 0
+                    self.dir_y = downside
+                    if D_req_E[0] == 0:
+                        self.mode = MOVE
+                    else:
+                        self.mode = CLEN
+                if D_req_E[1] == inf: # 오른쪽에 장애물이 있으면 아래쪽으로
+                    print("upside required energy is inf!")
+                    self.dir_x = 0
+                    self.dir_y = downside
+                    if D_req_E[0] == 0:
+                        self.mode = MOVE
+                    else:
+                        self.mode = CLEN          
+                else:
+                    self.dir_x = right_side # 아무것도 없으면 오른쪽으로
+                    self.dir_y = 0
+                    if D_req_E[0] == 0:
+                        self.mode = MOVE
+                    else:
+                        self.mode = CLEN
+            
+            if y == 16 and x > 0 and x < 16: # 아래쪽으로 끝까지 갔지만 모서리는 아닐 때
+                if D_req_E[2] == inf: # 왼쪽에 장애물이 있으면 위쪽으로
+                    print("downside required energy is inf!")
+                    self.dir_x = upside
+                    self.dir_y = 0
+                    if D_req_E[0] == 0:
+                        self.mode = MOVE
+                    else:
+                        self.mode = CLEN
+                if D_req_E[1] == inf: # 오른쪽에 장애물이 있으면 왼쪽으로
+                    print("upside required energy is inf!")
+                    self.dir_x = left_side
+                    self.dir_y = 0
+                    if D_req_E[0] == 0:
+                        self.mode = MOVE
+                    else:
+                        self.mode = CLEN   
+                else:
+                    self.dir_x = left_side
+                    self.dir_y = 0
+                    if D_req_E[0] == 0:
+                        self.mode = MOVE
+                    else:
+                        self.mode = CLEN        
 
-        if x == self.max_w - 2:
-            self.dir_x = 0
-            self.dir_y = 1
-            if grid_map.map[y][x].req_energy == 0:
-                self.mode = MOVE
+        if battery < 200:
+            if self.NOC == 3:
+                cleaning()
             else:
-                self.mode = CLEN
-
-        if x == self.max_w - 2 and y == self.max_h - 2:
-            self.dir_x = -1
-            self.dir_y = 0
-            if grid_map.map[y][x].req_energy == 0:
-                self.mode = MOVE
-            else:
-                self.mode = CLEN
-
-        if x == 2 and y == self.max_h - 2:
-            self.dir_x = 0
-            self.dir_y = -1
-            if grid_map.map[y][x].req_energy == 0:
-                self.mode = MOVE
-            else:
-                self.mode = CLEN
-
-        if x == 2 and y == 2:
-            self.dir_x = 1
-            self.dir_y = 0
+                print("battery: {}".format(battery))
+                battery_charge()
+        else: cleaning() 
         
         new_x = x + self.dir_x
         new_y = y + self.dir_y
-        '''if grid_map.map[new_y][new_x].req_energy == inf:
-            self.dir_x = 0
-            self.dir_y = 0'''
-
+        print("new_x : {}, new_y : {}".format(new_x, new_y))
         #END OF EXAMPLE CODE
 
         ######################
---
+++ python/random_test.py
@@ -0,0 +1,7 @@
+a = [1,2]
+print(a[0],a[1])
+b,c = a[0],a[1]
+print(b, c)
+print("hi")
+
+print(min(18,17,15))
