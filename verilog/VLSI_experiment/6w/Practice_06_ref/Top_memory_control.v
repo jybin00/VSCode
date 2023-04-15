8d9901505d4eda1e17de3e5dbcf6f9a0a6028f76
Yubeen Jo <jybin00@naver.com>
2023-04-15 17:22:56 +0900

    이제야 진짜 좀 알 것 같은데?

--- verilog/VLSI_experiment/6w/Practice_06_ref/Top_memory_control.v
+++ verilog/VLSI_experiment/6w/Practice_06_ref/Top_memory_control.v
@@ -17,13 +17,16 @@ module Top_memory_control(DATA_out, clk, rstn);
 
     counter_9b counter ({ADDR, State_counter}, clk, rstn);
     multiplier_csa_26b mul (out, DO[52-1:26], DO[25:0], clk, rstn);
-    rflp128x52mx4 MEMORY (DO, out, ADDR[6:2], ADDR[1:0], NWRT, NCE, clk);  // out은 클럭에 동기화 시키지 말고 바로 DIN하고 연결 해줘야 함.
+    rflp128x52mx4 MEMORY (DO, out, RA, CA, NWRT, NCE, clk);  // out은 클럭에 동기화 시키지 말고 바로 DIN하고 연결 해줘야 함.
     
     assign DATA_out = DO;
 
-    always@ (posedge rstn)
+    initial
     begin 
-        NCE <= 1'b0; NWRT <= 1'b1;
+        if(rstn == 1'b0)
+        begin
+            NCE <= 1'b0; NWRT <= 1'b1; {RA, CA} <= ADDR;
+        end
     end
 
     always@ (posedge clk)
@@ -31,10 +34,10 @@ module Top_memory_control(DATA_out, clk, rstn);
             if(rstn)
             begin
                 case(State_counter)
-                2'b00: begin NCE <= 1'b0; NWRT <= 1'b1;  end  // 주소 값은 어차피 NCE, NWRT에 의해 제어되므로
-                2'b01: begin NCE <= 1'b1; NWRT <= 1'b0;  end  // 계속 입력 값으로 넣어주어도 상관 X
-                2'b10: begin NCE <= 1'b1; NWRT <= 1'b0;  end  // 
-                2'b11: begin NCE <= 1'b0; NWRT <= 1'b0;  end
+                2'b00: begin NCE <= 1'b0; NWRT <= 1'b1; {RA, CA} <= ADDR; end  
+                2'b01: begin NCE <= 1'b1; NWRT <= 1'b0;  end  
+                2'b10: begin NCE <= 1'b1; NWRT <= 1'b0;  end   
+                2'b11: begin NCE <= 1'b0; NWRT <= 1'b0; {RA, CA} <= ADDR; end
  
                 endcase
             end
@@ -48,14 +51,10 @@ module counter_9b (cnt, clk, rstn);
     output [9-1:0] cnt;
     input  clk, rstn;
     reg [9-1:0] cnt;
-    always@ (posedge rstn)
-    begin
-        cnt <= 9'b0;
-    end
     
     always@ (posedge clk or rstn)
     begin
-        if (~rstn) begin cnt <= 9'x; end
+        if (~rstn) begin cnt <= 9'b0; end
         else if(cnt == 9'b111111111)
             begin
                 cnt <= 9'b0;
