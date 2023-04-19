9a443785fe043226247d1d54b14d7186de727e33
Yubeen Jo <jybin00@naver.com>
2023-04-20 00:08:42 +0900

    모든 영광은 준모님께,,

--- python/Information theory/Huffman_markov.py
+++ python/Information theory/Huffman_markov.py
@@ -1,7 +1,9 @@
 from collections import Counter  # 원소 카운트를 위한 라이브러리
+import heapq
 from heapq import heappush, heappop  # 최소값을 뽑기 위해서 힙 사용
 import numpy as np  # 배열 쉽게 다루기 위해 numpy사용
 import math
+import tree_class
 
 source_b = np.random.binomial(1, 1/2, size=1)  # Markov chain으로
 source_b = source_b.tolist()
@@ -21,7 +23,7 @@ One_prob = count_prob[1][1]/1000
 Zero_prob = count_prob[1][0]/1000
 
 source_entropy = Zero_prob*math.log(1/Zero_prob,2) + One_prob*math.log(1/One_prob,2)
-print("entropy bits:", source_entropy*1000)
+print("Origianl source entropy bits:", source_entropy*1000)
 
 # calculate empirical probabilities
 # 4개씩 끊어서 총 몇 번 나오는지 카운트
@@ -41,41 +43,79 @@ print("source extention entropy:", s_extention_entropy * 250)
 # build Huffman tree
 # 이렇게 넣으면 알아서 최상위 노드에 가장 작은 확률 값을 갖는 심볼이 들어감.
 # ""은 코드워드 넣을 공간
-heap = [[prob, [symbol, ""]] for symbol, prob in probabilities.items()]
-heap = sorted(heap)
-print(heap)
+heap = [tree_class.Node(symbol, prob) for symbol, prob in probabilities.items()]
+
+heapq.heapify(heap)
 # while문 돌려서 가장 작은 확률 값을 갖는 노드에 0 그 다음 노드에 1 부여하고 다시 힙에 넣어주기.
 print("build Huffman tree", "\n")
 while len(heap) > 1:
-    for i in range(len(heap)):
-        print(heap[i], sep='\n')
-    print('\n')
-    left = heappop(heap)
-    right = heappop(heap)
-    for pair in left[1:]:
-        # 비어 있는 칸에 코드워드를 넣어줌.
-        pair[1] = '0' + pair[1]
-    for pair in right[1:]:
-        pair[1] = '1' + pair[1]
-    heappush(heap, [left[0] + right[0]] + left[1:] + right[1:])
+    #for i in range(len(heap)):
+    #    print(heap[i], sep='\n')
+    #print('\n')
+    min0 = heappop(heap); min1 = heappop(heap)
+    min0.code = '0'; min1.code = '1'
+    total_prob = min0.freq + min1.freq
+    heappush(heap, tree_class.Node(None, total_prob, None, min0, min1))
 
 # generate codebook
-codebook = dict(heap[0][1:])
-print(heap[0])
-breakpoint()
+a = []
+def gen_codebook(node):
+    if node.flag == False:
+        if node.code != None:
+            if node.left:
+                node.left.code =  node.code + "0"
+                gen_codebook(node.left)
+            if node.right:
+                node.right.code = node.code + "1"
+                gen_codebook(node.right)
+            else:
+                node.flag = True
+                a.append((node.symbol, node.code))
+        else:
+            if node.left:
+                node.left.code = "0" 
+                gen_codebook(node.left)
+            if node.right:
+                node.right.code = "1" 
+                gen_codebook(node.right)
+    else:
+        pass
+
+gen_codebook(heap[0])
+a.sort(key=lambda e:len(e[1]))
+codebook = dict(a)
 print("Codebook")
 for key, value in codebook.items():
     print(key, value)
-print('\n')
-Huffman_tree = [[value, [key]] for key, value in codebook.items()]
-Huffman_tree = sorted(Huffman_tree)
-print("",Huffman_tree)
 print('\n')
 
 # encode sequence
 binary_string = "".join([codebook[tuple(source_b[i:i+4])] for i in range(0, len(source_b), 4)])
 
-print(binary_string,  sep='\n')
+decoded_bit = []
+head = heap[0]
+for i in range(0,len(binary_string)):
+    b = binary_string[i]
+    if b == '0':
+        head = head.left
+        if head.left is None and head.right is None: # leaf
+            decoded_bit.extend(list(head.symbol))
+            head = heap[0]
+    else:
+        head = head.right
+        if head.left is None and head.right is None: # leaf
+            decoded_bit.extend(list(head.symbol))
+            head = heap[0]
+            
+decoded_bit = [str(e) for e in decoded_bit]
+decoded_bit = ''.join(decoded_bit)
+source_b = ''.join([str(e)for e in source_b])
+print("Encoded_bit", binary_string ,sep='\n')
+print(decoded_bit == source_b)
+print(" \n")
+print("Decoded_bit", decoded_bit, sep='\n')
+
+print("Source bit:", source_b,  sep='\n')
 print('\n')
 
 print("Original source entropy bits:", math.ceil(source_entropy*1000))
--- python/Information theory/tree_class.py
+++ python/Information theory/tree_class.py
@@ -1,14 +1,14 @@
 class Node:
-    def __init__(self, symbol, freq, code=None, left = None, right = None):
+    def __init__(self, symbol, freq, code=None, left = None, right = None, flag=False ):
         self.symbol = symbol
         self.freq = freq
         self.code = code
         self.left = left
         self.right = right
+        self.flag = flag
 
     def __lt__(self, other):
         return self.freq < other.freq
 
 def isLeaf(root):
     return root.left is None and root.right is None
-    
