d495a0741c5449b8ed178c2808522d0f2508778a
Yubeen Jo <jybin00@naver.com>
2023-05-04 13:28:24 +0900

    made wire and counter

--- verilog/VLSI_experiment/2023_VLSI_Project1/2023_VLSI_Project1_ref/code/Top_controller.v
+++ verilog/VLSI_experiment/2023_VLSI_Project1/2023_VLSI_Project1_ref/code/Top_controller.v
@@ -10,11 +10,45 @@ module Top_controller (done, start, clk, rstn);
 
     output done;
     input start, clk, rstn;
+    reg flag;
 
-    rflp4096x8mx4 MEM_A(DO, DIN, RA, CA, NWRT, NCE, CLK);
-    rflp4096x8mx4 MEM_B(DO, DIN, RA, CA, NWRT, NCE, CLK);
-    rflp4096x22mx4 MEM_C(DO, DIN, RA, CA, NWRT, NCE, CLK);
+    wire [18-1:0] cnt_out;
+    counter_18b memory_controller(cnt_out, clk, rstn);
+    wire [8-1:0] Out_A, Out_B;
+    wire [22-1:0] Out_C;
+
+    wire [12-1:0] Addr_A, Addr_B, Addr_C;
+    assign Addr_A = {cnt_out[18-1:12], cnt_out[6-1:0]};
+    assign Addr_B = {cnt_out[6-1:0], cnt_out[12-1:6]};
+    assign Addr_C = cnt_out[18-1:6];
+
+    rflp4096x8mx4  MEM_A(Out_A, 8'b0, Addr_A[12-1:2], Addr_A[2-1:0], NWRT, NCE, clk);
+
+
+    rflp4096x8mx4  MEM_B(Out_B, 8'b0, Addr_B[12-1:2], Addr_B[2-1:0], NWRT, NCE, clk);
+
+
+    rflp4096x22mx4 MEM_C(Out_C, 22'b0, Addr_C[12-1:2], Addr_C[2-1:0], NWRT, NCE, clk);
+
+    always@(posedge clk)
+    begin
+        if(start == 1'b1) 
+
+
 
 
 endmodule
 
+
+// 18bits counter for memory controll 
+module counter_18b(cnt, clk, rstn);
+    output [18-1:0]cnt;
+    input clk, rstn;
+    reg [18-1:0]cnt;
+
+    always@(posedge clk)
+    begin
+        if(rstn == 1'b0) cnt <= 18'b0;
+        else cnt <= cnt + 1;
+    end
+endmodule
