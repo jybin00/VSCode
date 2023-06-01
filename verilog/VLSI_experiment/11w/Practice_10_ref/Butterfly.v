d17abecde7ab2a033bc43b2768b7bea7eca746ae
Yubeen Jo <jybin00@naver.com>
2023-06-01 19:32:10 +0900

    버터플라이 사실 b-a임.

--- verilog/VLSI_experiment/11w/Practice_10_ref/Butterfly.v
+++ verilog/VLSI_experiment/11w/Practice_10_ref/Butterfly.v
@@ -2,7 +2,7 @@
 // lattice 구조를 가지는 butterfly module을 설계하고, (진짜 butterfly모듈만, 주변 모듈 x)
 // 이를 이용하여 8 point FFT를 구현한다.
 module Butterfly(C1, C2, A, B);
-    output signed[24-1:0] C1, C2; // C1 = A + B, C2 = A - B
+    output signed[24-1:0] C1, C2; // C1 = A + B, C2 = B - A 근데 이렇게 하면 test vecot가 안 맞음. => 거꾸로 넣어줘야 할 듯.
     input signed[24-1:0] A, B; // A = A_r + j*A_i, B = B_r + j*B_i
 
     wire signed[12-1:0] A_r, A_i, B_r, B_i;  // A, B 자체는 합쳐져 있어서 signed bit 없지만,
@@ -13,7 +13,12 @@ module Butterfly(C1, C2, A, B);
     assign C1_r = A_r + B_r;  assign C1_i = A_i + B_i;
     assign C2_r = A_r - B_r;  assign C2_i = A_i - B_i;
 
-    assign C1 = {C1_r[13-1:1], C1_i[13-1:1]}; assign C2 = {C2_r[13-1:1], C2_i[13-1:1]};
+    wire signed[12-1:0] C1_rtmp, C1_itmp, C2_rtmp, C2_itmp;
+
+    assign C1_rtmp = C1_r[13-1:1]; assign C1_itmp = C1_i[13-1:1];
+    assign C2_rtmp = C2_r[13-1:1]; assign C2_itmp = C2_i[13-1:1];
+
+    assign C1 = {C1_rtmp, C1_itmp}; assign C2 = {C2_rtmp, C2_itmp};
 
 
 endmodule
