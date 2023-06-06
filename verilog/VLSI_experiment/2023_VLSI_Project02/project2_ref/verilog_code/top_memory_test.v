a6a411f3ef44c8f52a005f38c8c26c800d19a07e
Yubeen Jo <jybin00@naver.com>
2023-06-06 17:31:59 +0900

    메모리 아키텍쳐 구현

--- verilog/VLSI_experiment/2023_VLSI_Project02/project2_ref/verilog_code/top_memory_test.v
+++ verilog/VLSI_experiment/2023_VLSI_Project02/project2_ref/verilog_code/top_memory_test.v
@@ -1,10 +1,12 @@
 // stimulus에 들어가는 모듈
 `include "rflp16384x128mx16.v"
 `include "rfsp16384x192mx16.v"
-`include 
-module top_memory_test()
+`include "2D_DCT.v"
 
-rflp16384x128mx16 MEM_IN();
-rflp16384x192mx16 MEM_OUT();
+module top_memory_test(clk, reset)
+    input clk, reset;
+    
+    rflp16384x128mx16 MEM_IN();
+    rflp16384x192mx16 MEM_OUT();
 
 endmodule
